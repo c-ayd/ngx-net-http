c5f88cb060693fa42e27d4714bde59b8e5677f4b
c-ayd <201085822+c-ayd@users.noreply.github.com>
2025-04-02 18:11:38 +0200

    document DI function with comments

--- projects/ngx-net-http/src/lib/net-http-service-di.ts
+++ projects/ngx-net-http/src/lib/net-http-service-di.ts
@@ -1,5 +1,10 @@
 import { HttpFeature, HttpFeatureKind, provideHttpClient } from "@angular/common/http";
 
+/**
+ * Provides all dependencies for NetHttp.
+ * @param baseUrl Base URL for all request made by NetHttp.
+ * @param features HTTP features to be added to HttpClient.
+ */
 export function provideNetHttp(baseUrl?: string, ...features: HttpFeature<HttpFeatureKind>[]) {
     if (baseUrl)
         return [
